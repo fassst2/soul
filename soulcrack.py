4e87a4a04ba281b0011d7346daf022974cc55420
fassst2 <fasst1@proton.me>
2025-05-04 20:02:35 +0530

    Update soulcrack.py

--- soulcrack.py
+++ soulcrack.py
@@ -6,10 +6,10 @@ import datetime
 import os
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('8163671109:A')
+bot = telebot.TeleBot('6356732743:AAHJPGjHD0VPcb8UAFrAzsh6bMJCqJRvAGY')
 
 # Admin user IDs
-admin_id = {"80227", "12345667", "12345667"}
+admin_id = {"80227", "12345667", "933709537"}
 USER_FILE = "users1.txt"
 LOG_FILE = "log1.txt"
 
